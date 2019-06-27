32d6098ab657194d1df84e5222953314f370f840
jadephung <jadephung.29@gmail.com>
2019-06-27 18:02:40 +0700

    second homework

---
+++ src/TreeHeight.scala
@@ -0,0 +1,25 @@
+import scala.collection.mutable
+
+object TreeHeight {
+def main(args: Array[String]){
+//data input: array of data
+
+
+
+}
+
+  def countTheHeight(input : Array[Int]): Int ={
+var hashMapTree = new mutable.HashMap[Int, Int]
+    for (i <- 0 until input.length){
+      
+    }
+
+
+
+  }
+
+
+
+
+
+}
